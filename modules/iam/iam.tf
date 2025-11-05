7050c3deedc7c6f6b7b0c52d531513657d344f9c
Joel McFarlen <cjmcfarlen@gmail.com>
2025-11-05 11:49:52 -0600

    adding SSM to IAMfor troubleshooting

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -64,3 +64,8 @@ resource "aws_iam_role_policy_attachment" "node_ecr_ro" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+resource "aws_iam_role_policy_attachment" "node_ssm" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
